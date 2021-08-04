b6ac3fd58ed6351297e060a2b303499c80a479b1
Pankaj Kumar <pkumar70@slb.com>
2021-08-04 11:08:49 +0530

    Initial commit.

---
+++ product-catalog-svc/src/main/java/com/techdozo/product/ProductCatalogSvcApplication.java
@@ -0,0 +1,13 @@
+package com.techdozo.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogSvcApplication.class, args);
+	}
+
+}
